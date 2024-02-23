bc5ac3aae31f59a210dcae32eb92994c9d8b254a
Ivar <jeluzadrian07@gmail.com>
2024-02-23 19:30:39 +0800

    Added active styling to image marked as main.

--- codes/application/views/partials/uploaded_images.php
+++ codes/application/views/partials/uploaded_images.php
@@ -5,7 +5,7 @@ if(!isset($main_image) OR $main_image >= count($images)){
 if($images){
     foreach($images as $key => $image){?>
         <li>
-            <img src="<?=base_url('assets/images/uploads/'.$image)?>" alt="">
+            <img class="<?=($key == $main_image)? 'active' : '';?>" src="<?=base_url('assets/images/uploads/'.$image)?>" alt="">
             <label class=""><input type="checkbox" name="main_image" value="<?=$key?>" <?=($key == $main_image)? 'checked' : '';?>>Mark As Main</label>
             <button type="submit" class="delete_image" data-image-index="<?=$key?>">
         </li>                          
